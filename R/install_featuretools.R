2bcb859f7125b1efd1bbcdb62ea68ee58f38408c
Magnus Furugård <mafur90@gmail.com>
2019-08-04 13:52:30 +0200

    proper featuretools installation through virtualenv

--- R/install_featuretools.R
+++ R/install_featuretools.R
@@ -1,26 +1,35 @@
 #' Install featuretools
-#' @description Setup for featuretools in it's own virtualenv.
+#' @description Setup for featuretools in it's own virtualenv, or into the default reticulate virtualenv.
+#'
+#' @param custom_virtualenv Defaults to false. Set to true if you wish to use a custom virtualenv for featuretoolsR.
+#' @param method The installation method passed to `reticulate::py_install`. Defaults to "auto".
+#' @param conda Whether to use conda or not. Passed to `reticulate::py_install`. Defaults to "auto".
 #' @export
 #'
 #' @examples
 #' featuretoolsR::install_featuretools()
-install_featuretools <- function() {
+install_featuretools <- function(custom_virtualenv = F, method = "auto", conda = "auto") {
 
-  virtualenv_name <- getOption("featuretoolsR.virtualenv_name")
-  path <- paste(reticulate::virtualenv_root(), virtualenv_name, sep = "/")
-  if(!file.exists(path)) {
-    reticulate::virtualenv_create(virtualenv_name)
+  if(custom_virtualenv) {
+    virtualenv_name <- getOption("featuretoolsR.virtualenv_name")
+    path <- paste(reticulate::virtualenv_root(), virtualenv_name, sep = "/")
+    if(!file.exists(path)) {
+      reticulate::virtualenv_create(virtualenv_name)
+    } else {
+      message("Using existing virtualenv in ", path)
+    }
+
+    # Check if featuretools is installed
+    if(!reticulate::py_module_available("featuretools")) {
+      message("Installing featuretools into ", path)
+      # Install featuretools
+      reticulate::virtualenv_install(virtualenv_name, packages = "featuretools")
+    }
+
+    # Use new virtualenv
+    reticulate::use_virtualenv(virtualenv_name)
   } else {
-    message("Using existing virtualenv in ", path)
+    reticulate::py_install("featuretools", method = method, conda = conda)
   }
 
-  # Check if featuretools is installed
-  if(!reticulate::py_module_available("featuretools")) {
-    message("Installing featuretools into ", path)
-    # Install featuretools
-    reticulate::virtualenv_install(virtualenv_name, packages = "featuretools")
-  }
-
-  # Use new virtualenv
-  reticulate::use_virtualenv(virtualenv_name)
 }
